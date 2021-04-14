128a92b2f9b6fb4e84f14f6de426242917fc0679
Yoo Hyun Sik <dbgustlr92@gmail.com>
2021-04-14 13:30:12 +0900

    fix: 이모지들 표정외에 다른것들도 나오도록 적용

--- seep-ios/features/write/WriteReactor.swift
+++ seep-ios/features/write/WriteReactor.swift
@@ -154,9 +154,27 @@ class WriteReactor: Reactor {
   }
 
   private func generateRandomEmoji() -> String {
-    let range = 0x1F601...0x1F64F
-    let randomIndex = Int.random(in: range)
-    guard let scalar = UnicodeScalar(randomIndex) else { return "❓" }
+    let emojiArray = [
+      0x1f600...0x1f64f,
+      0x1f680...0x1f6c5,
+      0x1f6cb...0x1f6d2,
+      0x1f6e0...0x1f6e5,
+      0x1f6f3...0x1f6fa,
+      0x1f7e0...0x1f7eb,
+      0x1f90d...0x1f93a,
+      0x1f93c...0x1f945,
+      0x1f947...0x1f971,
+      0x1f973...0x1f976,
+      0x1f97a...0x1f9a2,
+      0x1f9a5...0x1f9aa,
+      0x1f9ae...0x1f9ca,
+      0x1f9cd...0x1f9ff,
+      0x1fa70...0x1fa73,
+      0x1fa78...0x1fa7a,
+      0x1fa80...0x1fa82,
+      0x1fa90...0x1fa95,
+    ].reduce([], +).map { return UnicodeScalar($0)! }
+    guard let scalar = emojiArray.randomElement() else { return "❓" }
     
     return String(scalar)
   }
