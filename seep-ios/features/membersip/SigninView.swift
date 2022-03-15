e6023eee2d2bcb8d53a264a3e88fe6c57701be3b
Hyunsik Yoo <dbgustlr92@gmail.com>
2022-03-15 22:54:15 +0900

    feat: 로그인 화면 뷰 그리기 완료

--- seep-ios/features/membersip/SigninView.swift
+++ seep-ios/features/membersip/SigninView.swift
@@ -1,6 +1,46 @@
 import UIKit
 
+import Gifu
+
 final class SigninView: BaseView {
+    private let gifView = GIFImageView().then {
+        $0.animate(withGIFNamed: "intro", loopCount: 0)
+    }
+    
+    private let titleLabel = UILabel().then {
+        let text = "membership_title".localized
+        let attributedString = NSMutableAttributedString(string: text)
+        let boldTextRange = (text as NSString).range(of: "하고 싶었던 것들")
+        let style = NSMutableParagraphStyle()
+        
+        style.maximumLineHeight = 34
+        style.minimumLineHeight = 34
+        
+        attributedString.addAttribute(
+            .font,
+            value: UIFont.appleBold(size: 26) as Any,
+            range: boldTextRange
+        )
+        attributedString.addAttribute(
+            .paragraphStyle,
+            value: style as Any,
+            range: (text as NSString).range(of: text)
+        )
+        $0.textColor = .gray5
+        $0.font = .appleLight(size: 26)
+        $0.attributedText = attributedString
+        $0.numberOfLines = 0
+        $0.textAlignment = .center
+    }
+    
+    private let descriptionLabel = UILabel().then {
+        $0.text = "membership_description".localized
+        $0.font = .appleRegular(size: 14)
+        $0.textColor = .gray4
+        $0.numberOfLines = 0
+        $0.setLineHeight(lineHeight: 20)
+        $0.textAlignment = .center
+    }
     let appleButton = SocialButton(socialType: .apple)
     
     let kakaoButton = SocialButton(socialType: .kakao)
@@ -14,6 +54,9 @@ final class SigninView: BaseView {
     override func setup() {
         self.backgroundColor = UIColor(r: 246, g: 247, b: 249)
         self.addSubViews([
+            self.gifView,
+            self.titleLabel,
+            self.descriptionLabel,
             self.appleButton,
             self.kakaoButton,
             self.startWithoutSigninButton
@@ -21,6 +64,22 @@ final class SigninView: BaseView {
     }
     
     override func bindConstraints() {
+        self.gifView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.bottom.equalTo(self.titleLabel.snp.top).offset(-16)
+            make.width.equalTo(240)
+            make.height.equalTo(240)
+        }
+        
+        self.titleLabel.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+        }
+        
+        self.descriptionLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(self.titleLabel.snp.bottom).offset(8)
+        }
+        
         self.startWithoutSigninButton.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.bottom.equalTo(self.safeAreaLayoutGuide).offset(-23)
